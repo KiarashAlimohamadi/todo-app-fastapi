1b8c80530b82b7d636d6bf81dc3df8a5cbce0be8
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2026-01-02 01:05:16 +0330

    CRUD functions

--- core/core/database.py
+++ core/core/database.py
@@ -13,4 +13,14 @@ SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 """
 create base class for declaring tables
 """
+
 Base = declarative_base()
+
+#---------------------------- CONNECTION TO MAIN.PY -------------------------------
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- core/tasks/routes.py
+++ core/tasks/routes.py
@@ -1,8 +1,14 @@
 #=================== IMPORTS ======================================
 
-from fastapi import APIRouter,Path
+from fastapi import APIRouter,Path,Depends,HTTPException
+from fastapi.responses import JSONResponse
 from tasks.schemas import *
 from tasks.models import TaskModel
+from sqlalchemy.orm import Session
+from core.database import get_db
+from typing import List
+from datetime import datetime
+
 
 #===================================================================
 
@@ -15,24 +21,62 @@ router = APIRouter(tags=["tasks"],prefix="/todo")
 
 #========================= FUNCTIONS ===============================
 
-@router.get("/tasks/")
-async def tasks_list():
-    return []
+@router.get("/tasks/",response_model=List[TaskResponseSchema])
+async def tasks_list(db:Session = Depends(get_db)):
+    result = db.query(TaskModel).all()
+    return result
 
-@router.get("/tasks/{task_id}")
-async def task_detail(task_id=int):
-    return task_id
 
-@router.post("/tasks")
-async def create_task():
-    return []
 
-@router.put("/tasks/{task_id}")
-async def update_task(task_id:int = Path(...,gt=0)):
-    return []
 
-@router.delete("tasks/{task_id}")
-async def delete_task(task_id:int = Path(...,gt=0)):
-    return []
+
+
+@router.get("/tasks/{task_id}",response_model=TaskResponseSchema)
+async def task_detail(task_id=int,db:Session = Depends(get_db)):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404,detail="Not Found!")
+    return task
+
+
+
+
+
+@router.post("/tasks",response_model=TaskResponseSchema)
+async def create_task(task:TaskCreateSchema,db:Session = Depends(get_db)):
+    new_task = TaskModel(title=task.title,description=task.description,is_completed=task.is_completed)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+
+
+
+
+@router.put("/tasks/{task_id}",response_model=TaskResponseSchema)
+async def update_task(task:TaskUpdateSchema,task_id:int = Path(...,gt=0),db:Session = Depends(get_db)):
+    task_obj = db.query(TaskModel).filter(TaskModel.id == task_id).first()
+    if not task_obj:
+        raise HTTPException(status_code=404, detail="Not Found!")
+    task_obj.title = task.title
+    task_obj.description = task.description
+    task_obj.is_completed = task.is_completed
+    now = datetime.now()
+    task_obj.updated_at = now
+    db.commit()
+    db.refresh(task_obj)
+    return task_obj
+
+
+
+
+@router.delete("tasks/{task_id}",status_code=204)
+async def delete_task(task_id:int = Path(...,gt=0),db:Session = Depends(get_db)):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404,detail="Not Found!")
+    db.delete(task)
+    db.commit()
+
 
 #===================================================================
