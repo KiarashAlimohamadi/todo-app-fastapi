8416e0aeaec131356a0cda8675abed87278476ed
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2026-01-02 02:15:26 +0330

    data base relations added and database bugs fixed

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -1,7 +1,8 @@
 #=========================== IMPORTS ======================
 
-from sqlalchemy import Column,Integer,String,Boolean,func,Text,DateTime
+from sqlalchemy import Column,Integer,String,Boolean,func,Text,DateTime,ForeignKey
 from core.database import Base
+from sqlalchemy.orm import relationship
 
 
 #==========================================================
@@ -12,8 +13,11 @@ from core.database import Base
 class TaskModel(Base):
     __tablename__ = "tasks"
     id = Column(Integer,autoincrement=True,primary_key=True)
+    user_id = Column(ForeignKey("users.id"))
     title = Column(String(150),nullable=False)
     description = Column(Text(500),nullable=True)
     is_completed = Column(Boolean,default=False)
     created_at = Column(DateTime,server_default=func.now())
     updated_at = Column(DateTime,server_default=func.now(),server_onupdate=func.now())
+
+    user = relationship("UserModel",back_populates="tasks",uselist=False)
--- core/users/models.py
+++ core/users/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column,String,TEXT,Boolean,func,Integer,DateTime
 from core.database import Base
+from sqlalchemy.orm import relationship
 
 
 
@@ -13,6 +14,8 @@ class UserModel(Base):
     is_active = Column(Boolean,default=True)
     created_at = Column(DateTime,server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+    tasks = relationship("TaskModel",back_populates="user")
 
     def __repr__(self):
         return (f'user : {self.id}')
