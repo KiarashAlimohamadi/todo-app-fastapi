510b53c1cf0b8aa2ec9e6dea20d6fbc5793f3fa7
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2026-01-01 23:39:07 +0330

    schemas created

---
+++ core/tasks/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel,Field
+from typing import Optional
+from datetime import datetime
+
+class TaskBaseSchema(BaseModel):
+    title : str = Field(...,max_length=150,min_length=5,description="Title of the Task")
+    description : Optional[str] = Field(None,max_length=500,description="Write something about your task")
+    is_completed : bool = Field(...,description="State of the task")
+
+class TaskCreateSchema(TaskBaseSchema):
+    pass
+class TaskUpdateSchema(TaskBaseSchema):
+    pass
+class TaskResponseSchema(TaskBaseSchema):
+    id : int = Field(...,description="id for the task(task number)")
+
+    created_date :datetime = Field(...,description="creation date and time of the task")
+    updated_date :datetime = Field(...,description="update date and time of the task")
