64f4b11502c8f4f91b9d5ffc2853d0dcd8e415ab
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2026-01-01 23:57:33 +0330

    routers base made

--- core/tasks/routes.py
+++ core/tasks/routes.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+#=================== IMPORTS ======================================
 
+from fastapi import APIRouter,Path
+from tasks.schemas import *
+from tasks.models import TaskModel
+
+#===================================================================
+
+
+#================ ROUTER SETUP =====================================
 router = APIRouter(tags=["tasks"],prefix="/todo")
+#===================================================================
 
+
+
+#========================= FUNCTIONS ===============================
 
 @router.get("/tasks/")
 async def tasks_list():
@@ -10,3 +22,17 @@ async def tasks_list():
 @router.get("/tasks/{task_id}")
 async def task_detail(task_id=int):
     return task_id
+
+@router.post("/tasks")
+async def create_task():
+    return []
+
+@router.put("/tasks/{task_id}")
+async def update_task(task_id:int = Path(...,gt=0)):
+    return []
+
+@router.delete("tasks/{task_id}")
+async def delete_task(task_id:int = Path(...,gt=0)):
+    return []
+
+#===================================================================
