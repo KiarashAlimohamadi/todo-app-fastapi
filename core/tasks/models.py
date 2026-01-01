e8cb887bbef06a05c137117c857f93abee25f024
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2026-01-01 20:04:52 +0330

    database config and settings created

---
+++ core/tasks/models.py
@@ -0,0 +1,10 @@
+#=========================== IMPORTS ======================
+
+from sqlalchemy import Column,Integer,String,Boolean,func
+from core.database import Base
+
+
+#==========================================================
+
+
+#================== MODELS =================================
