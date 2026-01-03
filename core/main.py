3f848a960c438bfbe3e3393848c57cf617ad93fb
Kiarash Alimohamadi <kiarashalimohamadi@gmail.com>
2026-01-03 23:07:28 +0330

    user authentication base function created

--- core/main.py
+++ core/main.py
@@ -1,18 +1,43 @@
+#========================== IMPORTS ========================
 
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.params import Depends
+from sqlalchemy.sql.annotation import Annotated
+from auth.basic_auth import get_authenticated_user
 from tasks.routes import router as tasks_routes
 from users.routes import router as users_route
+from users.models import UserModel
+
+#=============================================================
 
 
+
+#===================== LIFE SPAN =============================
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     print("started")
     yield
     print("shut down")
+#==============================================================
 
 
+
+#=============== SETUP =========================================
 app = FastAPI(lifespan=lifespan)
-
 app.include_router(tasks_routes)
 app.include_router(users_route)
+#==============================================================
+
+
+
+#================= FUNCTIONS ==================================
+@app.get("/public")
+def public_route():
+    return {"message":"public message"}
+
+@app.get("/private")
+def private_route(user:UserModel = Depends(get_authenticated_user)):
+    print(user)
+    return {"message":"private message"}
+#==============================================================
